fc8d72350b8a2b1faf91cbc7982472b89d2064f0
Rafael Moreira <rafael@rafaelmoreira.org>
2018-12-31 07:52:36 -0200

    add the symlink to the oh-my-zsh bootstrap

--- bootstrap/oh-my-zsh.sh
+++ bootstrap/oh-my-zsh.sh
@@ -4,3 +4,5 @@ ZSH=~/.oh-my-zsh
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH
 
 ln -s ~/dotfiles/zsh.ohmyzsh/.zshrc ~/.zshrc
+
+ln -s ~/dotfiles/zsh.custom/themes/spaceship-prompt/spaceship.zsh-theme ~/dotfiles/zsh.custom/themes/spaceship.zsh-theme
