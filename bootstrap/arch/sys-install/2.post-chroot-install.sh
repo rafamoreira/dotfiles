81fa196bd6a5a0990b54f3bf31cf53bd7595fa44
Rafael Moreira <rafael@rafaelmoreira.org>
2019-04-13 18:58:07 -0300

    Update 2.post-chroot-install.sh

--- bootstrap/arch/sys-install/2.post-chroot-install.sh
+++ bootstrap/arch/sys-install/2.post-chroot-install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Copenhagen /etc/localtime
 hwclock --systohc
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sed -i 's/^#pt_BR.UTF-8 UTF-8/pt_BR.UTF-8 UTF-8/' /etc/locale.gen
@@ -9,7 +9,7 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
 echo "127.0.0.1	localhost" >> /etc/hosts
 echo "::1 localhost" >> /etc/hosts
-echo "127.0.1.1 jupiter-arch.rafaelmoreira.org jupiter-arch" >> /etc/hosts
+echo "127.0.1.1 shakamaiden.localhost shakamaiden" >> /etc/hosts
 systemctl enable dhcpcd.service
 mkinitcpio -p linux
 passwd
@@ -29,7 +29,7 @@ elif [ $mtype == "physical" ]
 then
   # phys machine setup
   pacman -S vim intel-ucode grub efibootmgr
-  grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch
+  grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch
 fi
 sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
