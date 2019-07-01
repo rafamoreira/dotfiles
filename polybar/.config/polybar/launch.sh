0a48eb64d01ada772043c6341d28ad385bd1cfb8
Rafael Moreira <rafael@rafaelmoreira.org>
2019-07-01 02:55:23 -0300

    more confs to bspwm

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,7 +7,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar bar1
+polybar center &
+polybar left &
+polybar right
+
 # polybar bar2 &
 
 echo "Bars launched..."
