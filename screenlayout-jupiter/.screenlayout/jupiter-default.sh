2d427fe8e148b52cf1d1354747542f7459b5e4a9
Rafael Moreira <rafael@rafaelmoreira.org>
2019-07-01 02:11:19 -0300

    Add screenlayout default for desktop

---
+++ screenlayout-jupiter/.screenlayout/jupiter-default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 3840x2160 --pos 1920x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 5760x491 --rotate normal --output HDMI-A-1 --mode 1920x1080 --pos 0x491 --rotate normal --output DVI-D-0 --off
