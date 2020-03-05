97872ce452340b00e0c126795aefa04b3cafa92e
Rafael Moreira <rafael@rafaelmoreira.org>
2020-03-05 11:15:04 -0300

    add projectile to emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -26,12 +26,19 @@
   :config
   (evil-mode 1))
 
+(use-package projectile
+  :ensure t
+  :config
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key try use-package))))
+ '(package-selected-packages (quote (projectile which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
