24e5a44752fd3238d5b5f47cb26157d7ef637b96
Rafael M Cunha <me@rafaelmc.net>
2023-07-08 21:41:08 +0200

    add lualine to neovim

--- neovim/.config/nvim/lua/rmc/packer.lua
+++ neovim/.config/nvim/lua/rmc/packer.lua
@@ -132,4 +132,9 @@ require('packer').startup(function(use)
 	    "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
     }
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requres = { 'nvim-tree/nvim-web-devicons', opt = true },
+    }
 end)
