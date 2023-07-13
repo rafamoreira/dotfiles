662486a3a7e33e16d58848ab63d9dc7292ae70b1
Rafael M Cunha <me@rafaelmc.net>
2023-07-14 00:01:55 +0200

    remove neovim

--- neovim/.config/nvim/after/plugin/copilot.lua
+++
@@ -1,6 +0,0 @@
-vim.g.copilot_no_tab_map = true
--- vim.keymap.set({'i', 'n', 'v'}, '<C-j>', 'copilot#Accept("<CR>")'
-vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
-
-vim.keymap.set('n', '<leader>coe', '<cmd>Copilot enable<CR>')
-vim.keymap.set('n', '<leader>cod', '<cmd>Copilot disable<CR>')
