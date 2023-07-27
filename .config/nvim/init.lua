95eacec738fc93a3ea5bbf36d802cf13da72c7cc
Rafael M Cunha <me@rafaelmc.net>
2023-07-27 19:02:31 +0200

    change vim leader key

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,8 @@
 -- Set <,> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ','
-vim.g.maplocalleader = ','
+--  vim.g.mapleader = '\'
+--  vim.g.maplocalleader = '\'
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
