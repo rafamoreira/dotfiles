ab942dbce0b33776fe5fb9ce38c63e037f3162b8
Rafael M Cunha <me@rafaelmc.net>
2023-07-28 14:38:13 +0200

    correct i3 on titan

---
+++ .screenlayout/default-titan.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --primary --mode 1920x1080 --pos 2560x0 --rotate normal --output DisplayPort-0 --mode 2560x1440 --pos 4480x0 --rotate normal --output DisplayPort-1 --mode 2560x1440 --pos 0x0 --rotate normal
