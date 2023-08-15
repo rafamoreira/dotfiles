a5cedf4278a99c1d5b361ddd286a8e93b9fd51c5
Rafael M Cunha <me@rafaelmc.net>
2023-08-16 00:11:33 +0200

    neovim confs

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -68,11 +68,11 @@ require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
   -- Git related plugins
-  -- 'tpope/vim-fugitive',
-  -- 'tpope/vim-rhubarb',
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
 
   -- Detect tabstop and shiftwidth automatically
-  -- 'tpope/vim-sleuth',
+  'tpope/vim-sleuth',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
