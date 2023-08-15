64c01becfcb2ad61b9817d9b8aa5c06c98736ac8
Rafael M Cunha <me@rafaelmc.net>
2023-08-16 00:26:06 +0200

    org a bit

---
+++ neovim/.config/nvim/lua/custom/plugins/plugins.lua
@@ -0,0 +1,12 @@
+return {
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+  
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+
+  'github/copilot.vim',
+  "wakatime/vim-wakatime"
+}
