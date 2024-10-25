b80b156e0a50bc62b33846626252254d532483b8
Rafael M Cunha <me@rafaelmc.net>
2024-10-25 21:49:03 +0200

    NetworkManager

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 
 # This script is used to setup things that cannot be handle by stow
 sudo ln -sf $HOME/dotfiles/xorg/etc/X11/xorg.conf /etc/X11/xorg.conf
+
+sudo ln -sf $HOME/dotfiles/NetworkManager/conf.d/wifi-band-preference.conf /etc/NetworkManager/conf.d/wifi-band-preference.conf
