aec62ddd2b287dafcad5876652e159767cbf5637
Rafael M Cunha <me@rafaelmc.net>
2024-10-28 00:27:48 +0100

    network confs

--- system-config/arch-bootstrap/06.network.sh
+++ system-config/arch-bootstrap/06.network.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 systemctl enable --now systemd-networkd.service
+systemctl enable --now systemd-resolved.service
+
+# to use resolved we need to link /etc/resolv.conf to systemd-resolvd stub
+ln -sf ../run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
