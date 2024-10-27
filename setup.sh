ce43491107846d57ba7656357bafa14175cf4cbb
Rafael M Cunha <me@rafaelmc.net>
2024-10-27 17:37:43 +0100

    trying to move from network manager to systemd-networkd

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 # This script is used to setup things that cannot be handle by stow
 sudo ln -sf $HOME/dotfiles/xorg/etc/X11/xorg.conf /etc/X11/xorg.conf
 
-sudo ln -sf $HOME/dotfiles/NetworkManager/conf.d/wifi-band-preference.conf /etc/NetworkManager/conf.d/wifi-band-preference.conf
+# sudo ln -sf $HOME/dotfiles/NetworkManager/conf.d/wifi-band-preference.conf /etc/NetworkManager/conf.d/wifi-band-preference.conf
