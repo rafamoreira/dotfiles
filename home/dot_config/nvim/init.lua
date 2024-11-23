b672a74d2bccb24929ddb85c3f545cdec5bdbec4
Rafael M Cunha <me@rafaelmc.net>
2024-11-23 01:19:10 +0100

    neovim configs

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -25,13 +25,112 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- add your plugins here
+require("lazy").setup({ 
+	{ 
+		'nvim-telescope/telescope.nvim', 
+		dependencies = { 
+      'nvim-lua/plenary.nvim',
+      'sharkdp/fd',
+    },
+    config = function()
+      require('telescope').setup{
+        defaults = { 
+          -- Default configuration for telescope goes here: 
+          -- config_key = value,
+          mappings = {
+            i = {
+              -- map actions.which_key to <C-h> (default: <C-/>)
+              -- actions.which_key shows the mappings for your picker,
+              -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+              ["<C-h>"] = "which_key", 
+            },
+          }, 
+        },
+      }
+      local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    end,
+    pickers = {
+      -- Default configuration for builtin pickers goes here:
+      -- picker_name = {
+      --   picker_config_key = value,
+      --   ...
+      -- }
+      -- Now the picker_config_key will be applied every time you call this
+      -- builtin picker
+    },
+    extensions = {
+      -- Your extension configuration goes here:
+      -- extension_name = {
+      --   extension_config_key = value,
+      -- }
+      -- please take a look at the readme of the extension you want to configure
+    }
   },
+	{
+		"nvim-treesitter/nvim-treesitter", 
+		build = ":TSUpdate", 
+		config = function () 
+			local configs = require("nvim-treesitter.configs") 
+			configs.setup({ 
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" }, 
+				sync_install = false, 
+				highlight = { enable = true }, 
+				indent = { enable = true },  
+			}) 
+		end ,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = { 
+				{ 
+					"<leader>?", 
+					function() 
+						require("which-key").show({ global = false }) 
+					end, 
+					desc = "Buffer Local Keymaps (which-key)", 
+				}, 
+		}, 
+	}, 
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  { 
+    'andreasvc/vim-256noir',
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme '256_noir'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  { 'lucasprag/simpleblack' },
+
+  install = { colorscheme = { "tokyo-night" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+-- vim: ts=2 sts=2 sw=2 et
