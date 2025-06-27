4d7fb9493cc3d0853cd12d6873d867dde585593f
Rafael M Cunha <me@rafaelmc.net>
2025-06-27 22:02:02 +0200

    jupiter changes

--- home/dot_screenlayout/private_executable_default.sh
+++ home/dot_screenlayout/private_executable_default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-2 --primary --mode 2560x1440 --rate 75 --pos 2560x0 --rotate normal --output DP-1 --off --output HDMI-2 --mode 2560x1440 --rate 75 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
+xrandr --output HDMI-0 --primary --mode 2560x1440 --rate 75 --pos 0x0 --rotate normal --output DP-0 --mode 2560x1440 --rate 75 --pos 2560x0 --rotate normal 
