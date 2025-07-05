5b737a878b5ee439eaa0746c83f1333a65838544
Rafael M Cunha <me@rafaelmc.net>
2025-07-05 22:54:32 +0200

    add wezterm configs

---
+++ home/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,59 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 12
+config.color_scheme = 'tokyonight'
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
+
+-- timeout_milliseconds defaults to 1000 and can be omitted
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+  {
+    key = '|',
+    mods = 'LEADER|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+	{
+		key = '-',
+		mods = 'LEADER',
+		action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+	},
+  -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+  {
+    key = 'a',
+    mods = 'LEADER|CTRL',
+    action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
+  },
+}
+
+for i = 1, 8 do
+  -- CTRL+ALT + number to activate that tab
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'LEADER',
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+  -- F1 through F8 to activate that tab
+  table.insert(config.keys, {
+    key = 'F' .. tostring(i),
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
+
+-- Finally, return the configuration to wezterm:
+return config
+
